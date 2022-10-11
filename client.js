244e7d290b313f2488bee02e53da0ee95e7d069c
Schlüter <nikolaj.sn@hotmail.com>
2022-10-11 19:41:57 +0200

    danish instructions

--- client.js
+++ client.js
@@ -4,14 +4,14 @@ const client = dgram.createSocket('udp4');
 let timeStamp;
 const msg = 'ping';
 
-// Sending a message to the server
+// Sender en besked til serveren
 client.send(msg, 0, msg.length, 6790, '0.0.0.0', function onSendMessage() {
     timeStamp = Date.now()
     console.log('Sent', msg, 'at time:', timeStamp);
 });
 
 
-// Listening for incomming messages from the server
+// Lytter til svar fra serveren
 client.on('message', function (incommingMessage, remote) {
     console.log('Received', incommingMessage.toString());
 });
