123f66521cb1c6415214ede84d9e00b459d35c77
Schlüter <nikolaj.sn@hotmail.com>
2022-10-11 19:50:22 +0200

    Add new baseline

--- client.js
+++ client.js
@@ -1,17 +1,26 @@
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 
-let timeStamp;
-const msg = 'ping';
-
-// Sender en besked til serveren
-client.send(msg, 0, msg.length, 6790, '0.0.0.0', function onSendMessage() {
-    timeStamp = Date.now()
-    console.log('Sent', msg, 'at time:', timeStamp);
-});
-
+// client.send bruger callback (kedeeeligt), så vi konvertere den til promise for at kunne bruge async / await 
+function sendMessage(message, port, host) {
+    return new Promise((resolve, reject) => {
+        client.send(message, 0, message.length, port, host, (error, bytes) => {
+            if (error) {
+                return reject(error);
+            } 
+            return resolve(bytes);
+        })
+    })
+}
 
 // Lytter til svar fra serveren
 client.on('message', function (incommingMessage, remote) {
     console.log('Received', incommingMessage.toString());
 });
+
+
+// Sender en besked til serveren
+sendMessage('ping', 6790, '0.0.0.0').then(() => {
+    console.log('sent ping');
+})
+
