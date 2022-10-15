50af6f5eca42b1accdeb97d81eccc3d3defe8b50
Schlüter <nikolaj.sn@hotmail.com>
2022-10-15 23:17:43 +0200

    Add answer 1

--- client.js
+++ client.js
@@ -4,13 +4,13 @@ const client = dgram.createSocket('udp4');
 
 // Opgave 2:
 // DigitalOcean droplet i Frankfurt, Amsterdam, Singapore og San Francisco
-// const PORT = 6790;
-// const HOSTS = ['134.209.234.180', '167.172.32.197', '139.59.228.134', '161.35.224.80'];
 
 
 
 // Date.now() returner et UNIX timestamp (antal millisekunder siden 1. Januar 1970)
-var timestamp = Date.now();
+var timestamp = 0
+
+
 
 
 // client.send bruger callback (kedeligt), så vi konvertere den til promise for at kunne bruge async / await 
@@ -26,9 +26,13 @@ function sendMessage(message, port, host) {
 }
 
 
+
+const PORT = 6790;
+const HOSTS = ['134.209.234.180', '167.172.32.197', '139.59.228.134', '161.35.224.80'];
+
 // Sender en besked til serveren
 const message = 'ping'
-sendMessage(message, 6790, '0.0.0.0').then(() => {
+sendMessage(message, 6790, HOSTS[3]).then(() => { 
     timestamp = Date.now();
     console.log('sent', message, 'at time', timestamp);
 })
@@ -41,4 +45,7 @@ client.on('message', function (incommingMessage, remote) {
 
     // Opgave 1: Udregn svartiden fra serveren ved brug af Date.now()
     // ...
+    const receivedTime = Date.now();
+    const roundTripTime = receivedTime - timestamp;
+    console.log('Round trip time:', roundTripTime, 'ms');
 });
